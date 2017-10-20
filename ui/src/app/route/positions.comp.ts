7056f0723b6ce7fa2a203ccb46188108f54ed833
Micael Pedrosa <micaelpedrosa@gmail.com>
2017-10-20 18:57:25 +0100

    working with test data

--- ui/src/app/route/positions.comp.ts
+++ ui/src/app/route/positions.comp.ts
@@ -10,35 +10,62 @@ import * as d3 from 'd3';
 export class PositionsRoute {
   @ViewChild('chart') chart
 
-  height = 400
-  margin = 25
+  //scale configs
+  padding = { top: 20, right: 20, bottom: 20, left: 25 }
+
+  //scale conventions
+  width: number //= 800
+  height: number //= 500
+
+  innerWidth: number
+  innerHeight: number
 
   // Universidade de Aveiro -> 0300
   //   Engenharia de Computadores e Telemática -> 9361
   //   Engenharia Eletrónica e Telecomunicações -> 9365
-  // courses = [ { inst: '0300', code: '9361' }, { inst: '0300', code: '9365' }]
+  //   Engenharia Informática -> 9119
+  //   Engenharia Computacional -> G009
+  //   Tecnologias e Sistemas de Informação -> 9251
+
   institution = '0300'
-  courses = ['9361', '9365']
+  courses = ['9361', '9365', '9119', 'G009' , '9251']
   years = [2013, 2014, 2015, 2016]
 
+  //client filters
   yearSelection = 2015
 
+  data: any[]
+
+  //d3 fixed elements
   color: d3.ScaleLinear<string, string>
-  canvas: d3.Selection<any, {}, null, undefined>
+  svg: d3.Selection<any, {}, null, undefined>
+
+  //d3 elements dependent on data refresh
+  scaleX: d3.ScaleBand<string>
+  scaleY: d3.ScaleLinear<number, number>
+
+  //d3 elements dependent on resize and data refresh
+  courseBars: any
+  dataBars: any
 
   constructor(private qSrv: QueryService) {}
 
   ngAfterViewInit() {
-    this.color = d3.scaleLinear<string>()
-      .domain([0, 1, 2, 3, 4, 5])
-      .range(["red", "green", "blue", "purple", "yellow", "black"])
+    this.init()
+    this.resize()
 
-    this.canvas = d3.select(this.chart.nativeElement)
-      .attr("width", "100%")
-      .attr("height", this.height + 'px')
-  
-    this.getTestData()
-      .then(data => this.draw(this.transform(data)))
+    //redraw on resize - no data changes
+    window.onresize = (event) => {
+      this.resize()
+      this.redraw()
+    }
+
+    //refresh data
+    this.getTestData().then(results => {
+      this.data = this.transform(results)
+      this.refresh()
+      this.draw()
+    })
   }
 
   getTestData() {
@@ -55,34 +82,31 @@ export class PositionsRoute {
   getData() {
     let query = `
       MATCH (s:Student)-[:placed]->(a:Application)-[:on]->(c:Course)-[:of]->(i:Institution)
-      WHERE i.code = '${this.institution}' AND c.code IN [${this.courses.map(_ => "'"+_+"'")}] AND a.year IN [${this.years}] 
-      RETURN i.code AS institution, c.code AS course, a.year AS year, a.order AS option, count(DISTINCT s) AS placed
-      ORDER BY course
+      WHERE i.code = '${this.institution}' AND c.code IN [${this.courses.map(_ => "'"+_+"'")}] AND a.year IN [${this.years}]
+      WITH i.code AS institution, c.code AS course, a.year AS year, { option: a.order, placed: count(DISTINCT s) } AS options_sum
+      WITH institution, course, { year: year, options: collect(options_sum) } as years_col
+      RETURN institution, course, collect(years_col) as years
+      ORDER BY institution, course
     `
 
     return new Promise<any[]>((resolve, reject) => {
       this.qSrv.execQuery(query)
-        .subscribe(results => {
-          // group by course-code, year
-          let data = d3.nest()
-            .key((line: any) => line.institution + '-' + line.course)
-            .key((line: any) => line.year)
-            .entries(results as any[])
-              .map(course => {
-                let years = {}
-                course.values.forEach(yeaLine => {
-                  let year = []
-                  years[yeaLine.key] = year
-                  yeaLine.values.forEach(li => year[li.option - 1] = li.placed)
-                })
+        .subscribe((results: any[]) => {
+          let data = results.map(line => {
+            let years = {}
+            line.years.forEach(yeaLine => {
+              let year = []
+              years[yeaLine.year] = year
+              yeaLine.options.forEach(li => year[li.option - 1] = li.placed)
+            })
 
-                return {
-                  course: course.key,
-                  years: years
-                }
-              })
+            return {
+              course: line.institution + '-' + line.course,
+              years: years
+            }
+          })
           
-          resolve(data as any[])
+          resolve(data)
         }, error => reject(error))
     })
   }
@@ -98,60 +122,113 @@ export class PositionsRoute {
       }
     }))
 
+    console.log(results)
     return results
   }
 
-  draw(data: any[]) {
-    let barWidth = 57 //this.width / data.length
+  // process fixed d3 elements
+  init() {
+    console.log('INIT')
 
-    let max = d3.max(data, (course: any) => {
+    this.scaleX = d3.scaleBand()
+    this.scaleY = d3.scaleLinear()
+
+    this.color = d3.scaleLinear<string>()
+      .domain([0, 1, 2, 3, 4, 5])
+      .range(["red", "green", "blue", "purple", "yellow", "black"])
+
+    this.svg = d3.select(this.chart.nativeElement)
+      .append("g")
+        .attr("transform", `translate(${this.padding.left}, ${this.padding.top})`)
+      
+    this.svg.append("g").attr("class", "x-axis")
+    this.svg.append("g").attr("class", "y-axis")
+  }
+
+  // process d3 elements only related with data
+  refresh() {
+    console.log('REFRESH')
+
+    //max values of total placed 
+    let max = d3.max(this.data, (course: any) => {
       let sum = 0
-      course.years[this.yearSelection].forEach(placed => sum += placed.value)
+      let year: any = course.years[this.yearSelection]
+      if (year != null)
+        year.forEach(placed => sum += placed.value)
+
       return sum
     })
 
-    let scaleY = d3.scaleLinear()
-      .range([10, this.height])
-      .domain([0, max])
+    //xAxis labels from results
+    let xAxisKeys = this.data.map(line => line.course)
+
+    this.scaleX
+      .domain(xAxisKeys)
+      .padding(0.3)
     
-    let yAxis = d3.axisLeft(d3.scaleLinear().range([this.height, 10]).domain([0, max]))
+    this.scaleY
+      .domain([0, max])
+  }
 
-    this.canvas
-      .append("g")
-        .attr("class", "y-axis")
-        .attr("transform", `translate(${this.margin - 5}, -10)`)
-        .call(yAxis);
+  // process d3 elements related with new data
+  draw() {
+    console.log('DRAW')
 
-    let courseBar = this.canvas
-      .select('.chart')
-        .attr("transform", `translate(${this.margin}, ${this.height}) scale(1, -1)`)
-      .selectAll(".course")
-        .data(data)
-          .enter().append("g")
-            .attr("class", "course")
-            .attr("transform", (d, i) => `translate(${i*barWidth}, 0)`)
-
-    courseBar.selectAll(".placed")
+    this.courseBars = this.svg.selectAll(".course")
+      .data(this.data)
+        .enter().append("g")
+          .attr("class", "course")
+        
+    this.dataBars = this.courseBars.selectAll(".placed")
       .data((course: any) => course.years[this.yearSelection])
         .enter().append("rect")
           .attr("class", "placed")
-          .attr("y", (placed: any) => scaleY(placed.start))
-          .attr("height", (placed: any) => scaleY(placed.value))
-          .attr("width", barWidth - 7)
-          .attr("fill", (_, i) => this.color(i))
 
-    courseBar.exit().remove()
-    
-    /*courseBar.selectAll(".option")
-      .data((course: any) => [ course.years[this.yearSelection][0] ])
-        .enter().append("rect")
-          .attr("class", "option")
-          .attr("y", 0)
-          .attr("x", 51)
-          .attr("height", (placed: any) => scale(placed.value))
-          .attr("width", 5)
-          .attr("fill", "red")
-    */
+    this.redraw()
 
+    //will this work ?
+    this.courseBars.exit().remove()
+  }
+
+  // process width/height
+  resize() {
+    console.log('RESIZE')
+
+    this.width = window.innerWidth - 10
+    this.height = window.innerHeight - 100
+
+    this.innerWidth = this.width - this.padding.left - this.padding.right
+    this.innerHeight = this.height - this.padding.top - this.padding.bottom
+  }
+
+  // redraw d3 elements
+  redraw() {
+    console.log('REDRAW')
+
+    d3.select(this.chart.nativeElement)
+      .attr("width", this.width)
+      .attr("height", this.height)
+
+    this.scaleX.range([0, this.innerWidth])
+    this.scaleY.range([this.innerHeight, 0])
+
+    let xAxis = d3.axisBottom(this.scaleX)
+    let yAxis = d3.axisLeft(this.scaleY)
+
+    this.svg.select(".x-axis")
+      .attr("transform", `translate(0, ${this.innerHeight})`)
+      .call(xAxis)
+
+    this.svg.select(".y-axis")
+      .call(yAxis)
+
+    this.courseBars
+      .attr("transform", (line) => `translate(${this.scaleX(line.course)}, ${this.innerHeight}) scale(1, -1)`)
+
+    this.dataBars
+      .attr("y", (placed: any) => this.innerHeight - this.scaleY(placed.start))
+      .attr("height", (placed: any) => this.innerHeight - this.scaleY(placed.value))
+      .attr("width", this.scaleX.bandwidth())
+      .attr("fill", (_, i) => this.color(i))
   }
 }
